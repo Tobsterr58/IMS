7282e9692c1565e09ded7a3063bb491b94e50958
Tobsterr58 <tobste58@gmail.com>
2023-12-05 16:43:07 +0100

    Vypocitanie casu v rade

--- proj.cpp
+++ proj.cpp
@@ -42,13 +42,13 @@ enum typeOfWait {
   };
 
 // Definicia premennych pre pocet strojov pre jednotlive cvicenia a cviky a pocet klucov
-int kluce_gym = 140;
+int kluce_gym = 120;
 int kluce_squash = 20;
-int stroje_pull = 28;
-int stroje_push = 30;
-int stroje_legs = 36;
-int box_room = 8;
-int kardio_stroje = 20;
+int stroje_pull = 20;
+int stroje_push = 20;
+int stroje_legs = 32;
+int box_room = 6;
+int kardio_stroje = 15;
 int squash_kurt = 5;
 
 int pocet_zakaznikov = 0;
@@ -60,8 +60,19 @@ int pocet_zakaznikov_legs = 0;
 int pocet_zakaznikov_pull = 0;
 int pocet_zakaznikov_push = 0;
 
+// Definicia premennych pre pocet nespokojných zakaznikov a ID zakaznika
 int customer_id = 0;
 int nespokojny_zakaznik = 0;
+
+// Definicia premenných pre výpis času stráveného čakaním na niečo
+double cakanie_na_kurt = 0.0;
+double cakanie_na_kluc_gym = 0.0;
+double cakanie_na_kluc_squash = 0.0;
+double cakanie_na_stroj_pull = 0.0;
+double cakanie_na_stroj_push = 0.0;
+double cakanie_na_stroj_legs = 0.0;
+double cakanie_na_box = 0.0;
+double cakanie_na_kardio = 0.0;
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////// DEFINICIA STOROV  /////////////////////////////////////////////
@@ -79,8 +90,8 @@ Store SQUASH_KURTY ("Kurty na squash", squash_kurt);
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 class Timeout : public Event {
-  Process *ptr;        // který proces
-  typeOfWait type;    // typ čekání
+  Process *ptr;        // proces, ktorý čaká
+  typeOfWait type;    // typ čakania
   int ID;            // ID zakaznika
   public:
     Timeout(double t, Process *p, typeOfWait type, int ID): ptr(p), type(type), ID(ID) {
@@ -95,12 +106,15 @@ class Timeout : public Event {
       }
       delete ptr;        // likvidace
       if (type == GymKeys) {
+        cakanie_na_kluc_gym += NESPOKOJNY_ZAKAZNIK_GYM; 
         cout << "Zakaznik " << ID << " odisiel nespokojny lebo kluce gym" << endl;
       }
       else if (type == SquashKeys) {
+        cakanie_na_kluc_squash += NESPOKOJNY_ZAKAZNIK_SQUASH;
         cout << "Zakaznik " << ID << " odisiel nespokojny lebo kluce squash" << endl;
       }
       else if (type == Court) {
+        cakanie_na_kurt = cakanie_na_kurt + CAKANIE_NA_KURT;
         cout << "Zakaznik " << ID << " odisiel nespokojny lebo cakal na kurt pre squash moc dlho" << endl;
       }
 
@@ -118,52 +132,65 @@ class Customer : public Process {
     Prichod = Time; // Cas prichodu zakaznika
     pocet_zakaznikov++;
     Event *timeout;
-    float vyber_cvicenia;
+    double vyber_cvicenia;
 
     cout << "Prichod zakaznika: "<< ID << " v case : " << Prichod << endl;
     // Zadelenie zakaznika do fronty na gym alebo squash
-    if(Random() < 0.9) { // 90% zakaznikov ide na gym
+    if(Random() < 0.92) { // 92% zakaznikov ide na gym
       pocet_zakaznikov_gym++;
       cout << "Zakaznik " << ID << " ide do gymu" << endl;
       timeout = new Timeout(NESPOKOJNY_ZAKAZNIK_GYM, this, GymKeys, ID); // vytvoření události timeout
       cout << "Pocet volnych klucov na gym: " << GYM_KLUCE.Free() << endl;
+      double pomocnaGym = Time;
       Enter(GYM_KLUCE, 1); // Ziskanie kluca na gym
+      cakanie_na_kluc_gym += Time - pomocnaGym;
       delete timeout; // zrušení timeoutu
       Wait(Exponential(1)); // Cakanie na recepcii
 
       Wait(Uniform(BEFORE_CHANGE_ROOM_TIME_MIN, BEFORE_CHANGE_ROOM_TIME_MAX)); // Prezliekanie sa
       vyber_cvicenia = Random();
+      // 23% zakaznikov ide na pull, 23% na push, 36% na legs, 6% na box, 12% na kardio
       cout << "Zakaznik " << ID << " zacina cvicit v case :" << Time << endl;
       if (vyber_cvicenia < 0.23) {
+        double pomocnaPull = Time;
         Enter(PULL_STROJE, 1); // Ziskanie stroja na pull
+        cakanie_na_stroj_pull += Time - pomocnaPull;
         cout << "Zakaznik " << ID << " zacina cvicit pull" << endl;
         Wait(Uniform(MIN_TIME_PULL, MAX_TIME_PULL)); // Prebieha cvicenie
         Leave(PULL_STROJE, 1); // Vratenie stroja na pull
         pocet_zakaznikov_pull++;
       }
       else if (vyber_cvicenia > 0.23 && vyber_cvicenia < 0.46) {
+        double pomocnaPush = Time;
         Enter(PUSH_STROJE, 1); // Ziskanie stroja na push
+        cakanie_na_stroj_push += Time - pomocnaPush;
         cout << "Zakaznik " << ID << " zacina cvicit push" << endl;
         Wait(Uniform(MIN_TIME_PUSH, MAX_TIME_PUSH)); // Prebieha cvicenie
         Leave(PUSH_STROJE, 1); // Vratenie stroja na push
         pocet_zakaznikov_push++;
       }
       else if (vyber_cvicenia > 0.46 && vyber_cvicenia < 0.82) {
+        double pomocnaLegs = Time;
         Enter(LEGS_STROJE, 1); // Ziskanie stroja na legs
+        cakanie_na_stroj_legs += Time - pomocnaLegs;
         cout << "Zakaznik " << ID << " zacina cvicit legs" << endl;
         Wait(Uniform(MIN_TIME_LEGS, MAX_TIME_LEGS)); // Prebieha cvicenie
         Leave(LEGS_STROJE, 1); // Vratenie stroja na legs
         pocet_zakaznikov_legs++;
       }
       else if (vyber_cvicenia > 0.82 && vyber_cvicenia < 0.88) {
+        double pomocnaBox = Time;
         Enter(BOX_STROJE, 1); // Ziskanie stroja na box
+        cakanie_na_box += Time - pomocnaBox;
         cout << "Zakaznik " << ID << " zacina cvicit box" << endl;
         Wait(Uniform(MIN_TIME_BOX, MAX_TIME_BOX)); // Prebieha cvicenie
         Leave(BOX_STROJE, 1); // Vratenie stroja na box
         pocet_zakaznikov_box++;
       }
       else if (vyber_cvicenia > 0.88 && vyber_cvicenia < 1) {
+        double pomocnaKardio = Time;
         Enter(KARDIO_STROJE, 1); // Ziskanie stroja na kardio
+        cakanie_na_kardio += Time - pomocnaKardio;
         cout << "Zakaznik " << ID << " zacina cvicit kardio" << endl;
         Wait(Uniform(MIN_TIME_KARDIO, MAX_TIME_KARDIO)); // Prebieha cvicenie
         Leave(KARDIO_STROJE, 1); // Vratenie stroja na kardio
@@ -173,18 +200,23 @@ class Customer : public Process {
       Leave(GYM_KLUCE, 1); // Vratenie kluca na gym
       cout << "Zakaznik "<< ID << " skoncil cvicenie v case :" << Time << endl;
     }
-    else { // 10% zakaznikov ide na squash (2 zakaznici ako dvojica = 1 zakaznik pre squash)
+    else { // 8% zakaznikov ide na squash (2 zakaznici ako dvojica = 1 zakaznik pre squash)
       cout << "Dvojica " << ID << " ide na squash" << endl;
       pocet_zakaznikov_squash+=2;
       timeout = new Timeout(NESPOKOJNY_ZAKAZNIK_SQUASH, this, SquashKeys, ID); // vytvoření události timeout
       cout << "Pocet volnych klucov na squash: " << SQUASH_KLUCE.Free() << endl;
+      double pomocnaSquash = Time;
       Enter(SQUASH_KLUCE, 2); // Ziskanie klucov na squash
+      cakanie_na_kluc_squash += Time - pomocnaSquash;
       delete timeout; // zrušení timeoutu
       Wait(Exponential(1)); // Cakanie na recepcii
 
       Wait(Uniform(BEFORE_CHANGE_ROOM_TIME_MIN, BEFORE_CHANGE_ROOM_TIME_MAX)); // Prezliekanie sa
       timeout = new Timeout(CAKANIE_NA_KURT, this, Court, ID); // vytvoření události timeout
+      double pomocnaKurty = Time;
       Enter(SQUASH_KURTY, 1); // Ziskanie kurtu na squash
+      cout << "Zakaznik dostal kurt v case " << Time << endl;
+      cakanie_na_kurt += Time - pomocnaKurty;
       cout << "Dvojica " << ID << " zacina hrat squash v case :" << Time << endl;
       delete timeout; // zrušení timeoutu
       Wait(SQUASH_TIME); // Prebieha hra
@@ -202,7 +234,7 @@ class Generator : public Event {
     customer_id++;
     (new Customer(customer_id))->Activate();   // new customer
     if(Time < 420) { // 7:00 - 14:00 chodi menej zakaznikov kvôli škole, práci a pod.
-      Activate(Time+Exponential (3));
+      Activate(Time+Exponential (4));
     }
     else { // 14:00 - 22:00 chodi viac zakaznikov
       Activate(Time+Exponential (1));
@@ -212,8 +244,11 @@ class Generator : public Event {
 
 int main() {
 
+  // Create random seed
+  RandomSeed(time(NULL));
+
   // Initialize the simulation
-  Init(0, 900);
+  Init(0, 9000);
 
   // Activate the customer generator
   (new Generator)->Activate();
@@ -231,6 +266,14 @@ int main() {
   cout << "Pocet zakaznikov na legs: " << pocet_zakaznikov_legs << endl;
   cout << "Pocet zakaznikov na box: " << pocet_zakaznikov_box << endl;
   cout << "Pocet zakaznikov na kardio: " << pocet_zakaznikov_kardio << endl;
+  cout << "Zakaznici stravili cakanim na kurt: " << cakanie_na_kurt << endl;
+  cout << "Zakaznici stravili cakanim na kluc gym: " << cakanie_na_kluc_gym << endl;
+  cout << "Zakaznici stravili cakanim na kluc squash: " << cakanie_na_kluc_squash << endl;
+  cout << "Zakaznici stravili cakanim na stroj pull: " << cakanie_na_stroj_pull << endl;
+  cout << "Zakaznici stravili cakanim na stroj push: " << cakanie_na_stroj_push << endl;
+  cout << "Zakaznici stravili cakanim na stroj legs: " << cakanie_na_stroj_legs << endl;
+  cout << "Zakaznici stravili cakanim na box: " << cakanie_na_box << endl;
+  cout << "Zakaznici stravili cakanim na kardio: " << cakanie_na_kardio << endl;
 
   return 0;
 }
