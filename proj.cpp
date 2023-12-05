42e94e9789d64d479dbbdd12752dec7c78058fcf
Tobsterr58 <tobste58@gmail.com>
2023-12-05 19:14:26 +0100

    Added exercises

--- proj.cpp
+++ proj.cpp
@@ -11,12 +11,6 @@ using namespace std;
 /////////////////////////// DEFINICIA KONSTANT A CASOV  ///////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 
-#define MIN_TIME_PULL 60
-#define MAX_TIME_PULL 120
-#define MIN_TIME_PUSH 65
-#define MAX_TIME_PUSH 120
-#define MIN_TIME_LEGS 70
-#define MAX_TIME_LEGS 120
 #define MIN_TIME_BOX 60
 #define MAX_TIME_BOX 90
 #define MIN_TIME_KARDIO 45
@@ -29,6 +23,41 @@ using namespace std;
 #define NESPOKOJNY_ZAKAZNIK_GYM 20
 #define NESPOKOJNY_ZAKAZNIK_SQUASH 10
 #define CAKANIE_NA_KURT 30
+
+//////////////////////////////// LEGS EXERCISES //////////////////////////////////////////////////
+
+#define LEGS_EXERCISE_1_AVERAGE 15
+#define LEGS_EXERCISE_2_AVERAGE 10
+#define LEGS_EXERCISE_3_AVERAGE 10
+#define LEGS_EXERCISE_4_AVERAGE 10
+#define LEGS_EXERCISE_5_AVERAGE 10
+#define LEGS_EXERCISE_6_AVERAGE 10
+#define LEGS_EXERCISE_7_AVERAGE 10
+#define LEGS_EXERCISE_8_AVERAGE 8
+
+int LEGS_EXERCISES[8] = {LEGS_EXERCISE_1_AVERAGE, LEGS_EXERCISE_2_AVERAGE, LEGS_EXERCISE_3_AVERAGE, LEGS_EXERCISE_4_AVERAGE, LEGS_EXERCISE_5_AVERAGE, LEGS_EXERCISE_6_AVERAGE, LEGS_EXERCISE_7_AVERAGE, LEGS_EXERCISE_8_AVERAGE};
+
+//////////////////////////////// PUSH EXERCISES //////////////////////////////////////////////////
+
+#define PUSH_EXERCISE_1_AVERAGE 15
+#define PUSH_EXERCISE_2_AVERAGE 10
+#define PUSH_EXERCISE_3_AVERAGE 10
+#define PUSH_EXERCISE_4_AVERAGE 10
+#define PUSH_EXERCISE_5_AVERAGE 10
+
+int PUSH_EXERCISES[5] = {PUSH_EXERCISE_1_AVERAGE, PUSH_EXERCISE_2_AVERAGE, PUSH_EXERCISE_3_AVERAGE, PUSH_EXERCISE_4_AVERAGE, PUSH_EXERCISE_5_AVERAGE};
+
+//////////////////////////////// PULL EXERCISES //////////////////////////////////////////////////
+
+#define PULL_EXERCISE_1_AVERAGE 15
+#define PULL_EXERCISE_2_AVERAGE 10
+#define PULL_EXERCISE_3_AVERAGE 10
+#define PULL_EXERCISE_4_AVERAGE 10
+#define PULL_EXERCISE_5_AVERAGE 10
+
+int PULL_EXERCISES[5] = {PULL_EXERCISE_1_AVERAGE, PULL_EXERCISE_2_AVERAGE, PULL_EXERCISE_3_AVERAGE, PULL_EXERCISE_4_AVERAGE, PULL_EXERCISE_5_AVERAGE};
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
 
 enum typeOfWait {
   GymKeys,
@@ -44,10 +73,10 @@ enum typeOfWait {
 // Definicia premennych pre pocet strojov pre jednotlive cvicenia a cviky a pocet klucov
 int kluce_gym = 120;
 int kluce_squash = 20;
-int stroje_pull = 20;
-int stroje_push = 20;
-int stroje_legs = 32;
-int box_room = 6;
+int stroje_pull = 22;
+int stroje_push = 22;
+int stroje_legs = 34;
+int box_room = 7;
 int kardio_stroje = 15;
 int squash_kurt = 5;
 
@@ -102,7 +131,6 @@ class Timeout : public Event {
       if (type == Court) {
         ptr->Wait(Uniform(CHANGE_ROOM_AFTER_TRAINING, SHOWER_TIME_AFTER_TRAINING)); // Prezliekanie sa
         ptr->Leave(SQUASH_KLUCE, 2); // Vratenie klucov na squash
-        cout << "Dvojica " << ID << " vratila kluce od squashu" << endl;
       }
       delete ptr;        // likvidace
       if (type == GymKeys) {
@@ -134,13 +162,10 @@ class Customer : public Process {
     Event *timeout;
     double vyber_cvicenia;
 
-    cout << "Prichod zakaznika: "<< ID << " v case : " << Prichod << endl;
     // Zadelenie zakaznika do fronty na gym alebo squash
-    if(Random() < 0.92) { // 92% zakaznikov ide na gym
+    if(Random() < 0.95) { // 95% zakaznikov ide do gymu
       pocet_zakaznikov_gym++;
-      cout << "Zakaznik " << ID << " ide do gymu" << endl;
       timeout = new Timeout(NESPOKOJNY_ZAKAZNIK_GYM, this, GymKeys, ID); // vytvoření události timeout
-      cout << "Pocet volnych klucov na gym: " << GYM_KLUCE.Free() << endl;
       double pomocnaGym = Time;
       Enter(GYM_KLUCE, 1); // Ziskanie kluca na gym
       cakanie_na_kluc_gym += Time - pomocnaGym;
@@ -149,62 +174,59 @@ class Customer : public Process {
 
       Wait(Uniform(BEFORE_CHANGE_ROOM_TIME_MIN, BEFORE_CHANGE_ROOM_TIME_MAX)); // Prezliekanie sa
       vyber_cvicenia = Random();
-      // 23% zakaznikov ide na pull, 23% na push, 36% na legs, 6% na box, 12% na kardio
-      cout << "Zakaznik " << ID << " zacina cvicit v case :" << Time << endl;
-      if (vyber_cvicenia < 0.23) {
-        double pomocnaPull = Time;
-        Enter(PULL_STROJE, 1); // Ziskanie stroja na pull
-        cakanie_na_stroj_pull += Time - pomocnaPull;
-        cout << "Zakaznik " << ID << " zacina cvicit pull" << endl;
-        Wait(Uniform(MIN_TIME_PULL, MAX_TIME_PULL)); // Prebieha cvicenie
-        Leave(PULL_STROJE, 1); // Vratenie stroja na pull
+      // 25% zakaznikov cvici `Pull` cviky, 25% `Push` cviky, 31% `Legs` cviky, 6% `Box` cviky, 14% `Kardio` cviky
+      if (vyber_cvicenia < 0.25) {
+        for (int i = 0; i < 5; i++) {
+          double pomocnaPull = Time;
+          Enter(PULL_STROJE, 1); // Ziskanie stroja na pull
+          cakanie_na_stroj_pull += Time - pomocnaPull;
+          Wait(Uniform(PULL_EXERCISES[i]-2, PULL_EXERCISES[i]+8)); // Prebieha cvicenie
+          Leave(PULL_STROJE, 1); // Vratenie stroja na pull
+        }
         pocet_zakaznikov_pull++;
       }
-      else if (vyber_cvicenia > 0.23 && vyber_cvicenia < 0.46) {
-        double pomocnaPush = Time;
-        Enter(PUSH_STROJE, 1); // Ziskanie stroja na push
-        cakanie_na_stroj_push += Time - pomocnaPush;
-        cout << "Zakaznik " << ID << " zacina cvicit push" << endl;
-        Wait(Uniform(MIN_TIME_PUSH, MAX_TIME_PUSH)); // Prebieha cvicenie
-        Leave(PUSH_STROJE, 1); // Vratenie stroja na push
+      else if (vyber_cvicenia > 0.25 && vyber_cvicenia < 0.5) {
+        for (int i = 0; i < 5; i++) {
+          double pomocnaPush = Time;
+          Enter(PUSH_STROJE, 1); // Ziskanie stroja na push
+          cakanie_na_stroj_push += Time - pomocnaPush;
+          Wait(Uniform(PUSH_EXERCISES[i]-2, PUSH_EXERCISES[i]+8)); // Prebieha cvicenie
+          Leave(PUSH_STROJE, 1); // Vratenie stroja na push
+        }
         pocet_zakaznikov_push++;
       }
-      else if (vyber_cvicenia > 0.46 && vyber_cvicenia < 0.82) {
-        double pomocnaLegs = Time;
-        Enter(LEGS_STROJE, 1); // Ziskanie stroja na legs
-        cakanie_na_stroj_legs += Time - pomocnaLegs;
-        cout << "Zakaznik " << ID << " zacina cvicit legs" << endl;
-        Wait(Uniform(MIN_TIME_LEGS, MAX_TIME_LEGS)); // Prebieha cvicenie
-        Leave(LEGS_STROJE, 1); // Vratenie stroja na legs
+      else if (vyber_cvicenia > 0.5 && vyber_cvicenia < 0.8) {
+        for (int i = 0; i < 8; i++) {
+          double pomocnaLegs = Time;
+          Enter(LEGS_STROJE, 1); // Ziskanie stroja na legs
+          cakanie_na_stroj_legs += Time - pomocnaLegs;
+          Wait(Uniform(LEGS_EXERCISES[i]-2, LEGS_EXERCISES[i]+8)); // Prebieha cvicenie
+          Leave(LEGS_STROJE, 1); // Vratenie stroja na legs
+        }
         pocet_zakaznikov_legs++;
       }
-      else if (vyber_cvicenia > 0.82 && vyber_cvicenia < 0.88) {
+      else if (vyber_cvicenia > 0.8 && vyber_cvicenia < 0.86) {
         double pomocnaBox = Time;
         Enter(BOX_STROJE, 1); // Ziskanie stroja na box
         cakanie_na_box += Time - pomocnaBox;
-        cout << "Zakaznik " << ID << " zacina cvicit box" << endl;
         Wait(Uniform(MIN_TIME_BOX, MAX_TIME_BOX)); // Prebieha cvicenie
         Leave(BOX_STROJE, 1); // Vratenie stroja na box
         pocet_zakaznikov_box++;
       }
-      else if (vyber_cvicenia > 0.88 && vyber_cvicenia < 1) {
+      else if (vyber_cvicenia > 0.86 && vyber_cvicenia < 1) {
         double pomocnaKardio = Time;
         Enter(KARDIO_STROJE, 1); // Ziskanie stroja na kardio
         cakanie_na_kardio += Time - pomocnaKardio;
-        cout << "Zakaznik " << ID << " zacina cvicit kardio" << endl;
         Wait(Uniform(MIN_TIME_KARDIO, MAX_TIME_KARDIO)); // Prebieha cvicenie
         Leave(KARDIO_STROJE, 1); // Vratenie stroja na kardio
         pocet_zakaznikov_kardio++;
       }
       Wait(Uniform(CHANGE_ROOM_AFTER_TRAINING, SHOWER_TIME_AFTER_TRAINING)); // Prezliekanie sa
       Leave(GYM_KLUCE, 1); // Vratenie kluca na gym
-      cout << "Zakaznik "<< ID << " skoncil cvicenie v case :" << Time << endl;
     }
-    else { // 8% zakaznikov ide na squash (2 zakaznici ako dvojica = 1 zakaznik pre squash)
-      cout << "Dvojica " << ID << " ide na squash" << endl;
+    else { // 5% zakaznikov ide na squash (2 zakaznici ako dvojica = 1 zakaznik pre squash)
       pocet_zakaznikov_squash+=2;
       timeout = new Timeout(NESPOKOJNY_ZAKAZNIK_SQUASH, this, SquashKeys, ID); // vytvoření události timeout
-      cout << "Pocet volnych klucov na squash: " << SQUASH_KLUCE.Free() << endl;
       double pomocnaSquash = Time;
       Enter(SQUASH_KLUCE, 2); // Ziskanie klucov na squash
       cakanie_na_kluc_squash += Time - pomocnaSquash;
@@ -215,13 +237,10 @@ class Customer : public Process {
       timeout = new Timeout(CAKANIE_NA_KURT, this, Court, ID); // vytvoření události timeout
       double pomocnaKurty = Time;
       Enter(SQUASH_KURTY, 1); // Ziskanie kurtu na squash
-      cout << "Zakaznik dostal kurt v case " << Time << endl;
       cakanie_na_kurt += Time - pomocnaKurty;
-      cout << "Dvojica " << ID << " zacina hrat squash v case :" << Time << endl;
       delete timeout; // zrušení timeoutu
       Wait(SQUASH_TIME); // Prebieha hra
       Leave(SQUASH_KURTY, 1); // Vratenie kurtu na squash
-      cout << "Dvojica " << ID << " skoncila hrat squash v case :" << Time << endl;
       Wait(Uniform(CHANGE_ROOM_AFTER_TRAINING, SHOWER_TIME_AFTER_TRAINING)); // Prezliekanie sa
       Leave(SQUASH_KLUCE, 2); // Vratenie klucov na squash
     }
@@ -248,7 +267,7 @@ int main() {
   RandomSeed(time(NULL));
 
   // Initialize the simulation
-  Init(0, 9000);
+  Init(0, 900);
 
   // Activate the customer generator
   (new Generator)->Activate();
@@ -261,11 +280,11 @@ int main() {
   cout << "Pocet zakaznikov gym: " << pocet_zakaznikov_gym << endl;
   cout << "Pocet zakaznikov squash: " << pocet_zakaznikov_squash << endl;
   cout << "Pocet nespokojnych zakaznikov: " << nespokojny_zakaznik << endl;
-  cout << "Pocet zakaznikov na pull: " << pocet_zakaznikov_pull << endl;
-  cout << "Pocet zakaznikov na push: " << pocet_zakaznikov_push << endl;
-  cout << "Pocet zakaznikov na legs: " << pocet_zakaznikov_legs << endl;
-  cout << "Pocet zakaznikov na box: " << pocet_zakaznikov_box << endl;
-  cout << "Pocet zakaznikov na kardio: " << pocet_zakaznikov_kardio << endl;
+  // cout << "Pocet zakaznikov na pull: " << pocet_zakaznikov_pull << endl;
+  // cout << "Pocet zakaznikov na push: " << pocet_zakaznikov_push << endl;
+  // cout << "Pocet zakaznikov na legs: " << pocet_zakaznikov_legs << endl;
+  // cout << "Pocet zakaznikov na box: " << pocet_zakaznikov_box << endl;
+  // cout << "Pocet zakaznikov na kardio: " << pocet_zakaznikov_kardio << endl;
   cout << "Zakaznici stravili cakanim na kurt: " << cakanie_na_kurt << endl;
   cout << "Zakaznici stravili cakanim na kluc gym: " << cakanie_na_kluc_gym << endl;
   cout << "Zakaznici stravili cakanim na kluc squash: " << cakanie_na_kluc_squash << endl;
